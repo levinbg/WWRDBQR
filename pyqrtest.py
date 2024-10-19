36436443640e2edb0d2707eb41a4395068009cb5
Brian Levin <levinbg@fan.gov>
2024-10-19 09:57:29 -0400

    Working on reframing interface

--- pyqrtest.py
+++ pyqrtest.py
@@ -50,53 +50,60 @@ if __name__ == "__main__":
     # Configure the window to use the menu bar
     window.config(menu=menubar)
     
-    label_make = tk.Label(frame, text="Make", height=1)
+    input_frame = tk.Frame(frame)
+    
+    label_make = tk.Label(input_frame, text="Make", height=1)
     label_make.grid(column=0, row=0)
-    input_make = tk.Text(frame, width=10, height=1)
+    input_make = tk.Text(input_frame, width=10, height=1)
     input_make.grid(column=1, row=0)
     
-    label_model = tk.Label(frame, text="Model", height=1)
+    label_model = tk.Label(input_frame, text="Model", height=1)
     label_model.grid(column=0, row=2)
-    input_model = tk.Text(frame, width=10, height=1)
+    input_model = tk.Text(input_frame, width=10, height=1)
     input_model.grid(column=1, row=2)
     
-    label_system = tk.Label(frame, text="System", height=1)
+    label_system = tk.Label(input_frame, text="System", height=1)
     label_system.grid(column=0, row=3)
-    input_system = tk.Text(frame, width=10, height=1)
+    input_system = tk.Text(input_frame, width=10, height=1)
     input_system.grid(column=1, row=3)
     
-    label_P1 = tk.Label(frame, text="P1", height=1)
+    label_P1 = tk.Label(input_frame, text="P1", height=1)
     label_P1.grid(column=0, row=4)
-    input_P1 = tk.Text(frame, width=10, height=1)
+    input_P1 = tk.Text(input_frame, width=10, height=1)
     input_P1.grid(column=1, row=4)
     
-    label_P2 = tk.Label(frame, text="P2", height=1)
+    label_P2 = tk.Label(input_frame, text="P2", height=1)
     label_P2.grid(column=0, row=5)
-    input_P2 = tk.Text(frame, width=10, height=1)
+    input_P2 = tk.Text(input_frame, width=10, height=1)
     input_P2.grid(column=1, row=5)
     
-    label_P3 = tk.Label(frame, text="P3", height=1)
+    label_P3 = tk.Label(input_frame, text="P3", height=1)
     label_P3.grid(column=0, row=6)
-    input_P3 = tk.Text(frame, width=10, height=1)
+    input_P3 = tk.Text(input_frame, width=10, height=1)
     input_P3.grid(column=1, row=6)
     
-    label_P4 = tk.Label(frame, text="P4", height=1)
+    label_P4 = tk.Label(input_frame, text="P4", height=1)
     label_P4.grid(column=0, row=7)
-    input_P4 = tk.Text(frame, width=10, height=1)
+    input_P4 = tk.Text(input_frame, width=10, height=1)
     input_P4.grid(column=1, row=7)
     
-    label_P5 = tk.Label(frame, text="P5", height=1)
+    label_P5 = tk.Label(input_frame, text="P5", height=1)
     label_P5.grid(column=0, row=8)
-    input_P5 = tk.Text(frame, width=10, height=1)
+    input_P5 = tk.Text(input_frame, width=10, height=1)
     input_P5.grid(column=1, row=8)
     
+    input_frame.pack()
+    # input_frame.grid(column=0, row=0)
+    
+    
      # Display QR Code
-    max_row = frame.grid_size()[1]
+    qr_frame = tk.Frame(frame)
     qr = create_qr(data)
     tkqr = ImageTk.PhotoImage(qr)
-    label = tk.Label(frame, image=tkqr)
+    label = tk.Label(qr_frame, image=tkqr)
     label.image = tkqr
-    label.grid(column=2, row=0, rowspan=max_row, ipadx=20, ipady=20, sticky="se")
+    qr_frame.pack()
+    # qr_frame.grid(column=1, row=0)
 
     # Start the GUI
     window.mainloop()
